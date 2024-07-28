f2a13d8d9167df6ea2c0c80251e213bf46fc23c8
satishrongali <rongalisatish240@gmail.com>
2024-07-28 15:52:00 +0200

    Implemented Firebase in frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8081'
+    //apiUrl: 'http://localhost:8081'
+    apiUrl: 'https://us-central1-satish-masterthesis.cloudfunctions.net/api'
   };
   
